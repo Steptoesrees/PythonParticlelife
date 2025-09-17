83fd35211ff908eda75ea42115dfa34b316414b7
steptoerees <Leonhsouthall@gmail.com>
2025-09-17 12:41:34 +0100

    saved

--- particle_Life_OOP/Camera.py
+++ particle_Life_OOP/Camera.py
@@ -13,11 +13,13 @@ class Camera:
         self.cam.fill((0,0,0))
         for i in range(len(physics.particles)):
             physics.particles[i].drawParticle(self.cam, self.cam_size)
-            screen.blit(self.cam, (self.cam_x,self.cam_y))
+        screen.blit(self.cam, (self.cam_x,self.cam_y))
+        
             
 
     def zoomIn(self, zoom_amount):
         self.cam_size += 50 * abs(zoom_amount)
+        self.cam_size = round(self.cam_size, 1)
         self.cam = pygame.Surface((self.cam_size,self.cam_size))
     
     def zoomOut(self, zoom_amount):
@@ -27,6 +29,7 @@ class Camera:
         self.cam = pygame.Surface((self.cam_size,self.cam_size))
 
     def pan(self, dx, dy):
-        self.cam_x += dx
-        self.cam_y += dy
+        if self.dragging:
+            self.cam_x += dx
+            self.cam_y += dy
         
--- particle_Life_OOP/Menu.py
+++ particle_Life_OOP/Menu.py
@@ -5,6 +5,7 @@ class Menu:
     def __init__(self,screen, screen_width, screen_height, physics):
         self.screen = screen
 
+        self.sliderdrag =  False
 
         self.menu = pygame.Surface((screen_height/2,screen_height))
         self.menu_rect = self.menu.get_rect() #for collision
@@ -15,12 +16,18 @@ class Menu:
         self.screen_height = screen_height
         self.create_sliders()
 
-        self.force_graph = ForceGraph(self.menu, 0, 40, screen_height/4, screen_height/2, physics)
+        self.physics_engine = physics
+
+        self.force_graph = ForceGraph(self.menu, 0, 20, screen_height/4, screen_height/2, physics)
 
         self.create_buttons()
+
         
     def mouse_in_menu(self, mousepos):
-        return self.menu_rect.collidepoint(mousepos)
+        if self.visible:
+            return self.menu_rect.collidepoint(mousepos)
+        else:
+            return False
 
     def draw(self):
         self.menu.fill((43,42,51))
@@ -30,6 +37,7 @@ class Menu:
                 
             else:
                 pass
+            self.update_slider_values()
             self.draw_sliders()
             self.draw_buttons()
             self.screen.blit(self.menu, (0,0))
@@ -41,7 +49,7 @@ class Menu:
             self.graph_or_matrix = 1
 
     def create_buttons(self):
-        self.graph_matrix_button = Button((100,100,100), (self.screen_height/2)-80,(self.screen_height/4), 80, 40, self.menu, self.toggle_graph_matrix, None, text="G/M")
+        self.graph_matrix_button = Button((100,100,100), (self.screen_height/2)-80,(self.screen_height/4)-20, 80, 40, self.menu, self.toggle_graph_matrix, None, text="G/M")
 
     def draw_buttons(self):
         self.graph_matrix_button.draw()
@@ -51,14 +59,41 @@ class Menu:
 
     def create_sliders(self):
         pos = (10, 40 + self.screen_height/4 + 20)
-        self.slider = Sliders((pos[0],pos[1]), whole_width=self.screen_height/2 - 20)
-        self.slider2 = Sliders((pos[0],pos[1]+50), whole_width=self.screen_height/2 - 20, neg=True)
+        self.particle_num_slider = Sliders(self, (pos[0],pos[1]), whole_width=self.screen_height/2 - 20, max_val=400, slider_val=300, Name='Particles:')
+        self.force_factor_slider = Sliders(self, (pos[0],pos[1]+50), whole_width=self.screen_height/2 - 20, neg=True, max_val=20, slider_val=10, Name='Force Factor: ')
+        self.beta_slider = Sliders(self, (pos[0],pos[1]+100), whole_width=self.screen_height/2 - 20, max_val=1, slider_val=0.25, Name='Beta: ')
+        self.radius_slider = Sliders(self, (pos[0],pos[1]+150), whole_width=self.screen_height/2 - 20, max_val=1, slider_val=0.25, Name='Radius: ')
 
     def draw_sliders(self):
-        self.slider.draw(self.menu)
-        self.slider2.draw(self.menu)
+        self.particle_num_slider.draw(self.menu)
+        self.force_factor_slider.draw(self.menu)
+        self.beta_slider.draw(self.menu)
+        self.radius_slider.draw(self.menu)
 
-        
+    def slide(self, mousepos, dx):
+        self.particle_num_slider.button_click(mousepos, dx)
+        self.force_factor_slider.button_click(mousepos, dx)
+        self.beta_slider.button_click(mousepos, dx)
+        self.radius_slider.button_click(mousepos, dx)
+
+        self.physics_engine.changeParticleCount(int(self.particle_num_slider.slider_value))
+        self.physics_engine.force_Factor = self.force_factor_slider.slider_value
+        self.physics_engine.beta = self.beta_slider.slider_value
+        self.physics_engine.max_Radius = self.radius_slider.slider_value
+
+    def update_slider_values(self):
+
+        self.particle_num_slider.slider_value = len(self.physics_engine.particles)+1
+        self.force_factor_slider.slider_value = self.physics_engine.force_Factor
+        self.beta_slider.slider_value = self.physics_engine.beta
+        self.radius_slider.slider_value = self.physics_engine.max_Radius
+
+
+        self.particle_num_slider.updateslider()
+        self.force_factor_slider.updateslider()
+        self.beta_slider.updateslider()
+        self.radius_slider.updateslider()
+
 class Taskbar():
 
     def __init__(self,screen, screen_width, screen_height, menu, physics_engine):
@@ -68,17 +103,17 @@ class Taskbar():
         self.physics_engine = physics_engine
 
         self.screen = screen
-        self.taskbar = pygame.Surface((screen_width, 40))
+        self.taskbar = pygame.Surface((screen_width, 20))
         self.taskbar_rect = self.taskbar.get_rect() #for collision
         print(self.physics_engine)
 
         
-        self.menu_button = Button((100,100,100), 0, 0, 40, 40, self.taskbar, self.toggle_menu, None,text='M')
-        self.add_particle_button = Button((100,100,100), 100, 0, 100, 40, self.taskbar, self.physics_engine.addParticle, None,text='+')
-        self.remove_particle_button = Button((100,100,100), 210, 0, 100, 40, self.taskbar, self.physics_engine.removeParticle, None,text='-')
-        self.reset_matrix_button = Button((100,100,100), 320, 0, 100, 40, self.taskbar, self.physics_engine.randomMatrix, None, text='R')
-        self.pause_button = Button((100,100,100), 430, 0, 100, 40, self.taskbar, self.pause_simulation, None,text='Pause')
-        self.exit_button = Button((200,0,0), self.screen_width-40, 0, 40, 40, self.taskbar, self.exit, None,text='×')
+        self.menu_button = Button((100,100,100), 0, 0, 40, 20, self.taskbar, self.toggle_menu, None,text='M')
+        self.add_particle_button = Button((100,100,100), 100, 0, 100, 20, self.taskbar, self.physics_engine.addParticle, None,text='+')
+        self.remove_particle_button = Button((100,100,100), 210, 0, 100, 20, self.taskbar, self.physics_engine.removeParticle, None,text='-')
+        self.reset_matrix_button = Button((100,100,100), 320, 0, 100, 20, self.taskbar, self.physics_engine.randomMatrix, None, text='R')
+        self.pause_button = Button((100,100,100), 430, 0, 100, 20, self.taskbar, self.pause_simulation, None,text='Pause')
+        self.exit_button = Button((200,0,0), self.screen_width-40, 0, 40, 20, self.taskbar, self.exit, None,text='×')
 
 
     def draw(self):
@@ -182,7 +217,7 @@ class ForceGraph():
         maxy_label = self.font.render('1', True, (255,255,255))
 
         #draw labels
-        self.graph.blit(x_label, (self.width//2 - x_label.get_width()//2, self.height-30))
+        self.graph.blit(x_label, (self.width//2 - x_label.get_width()//2, self.height-60))
         self.graph.blit(y_label, (20, self.height//2 - y_label.get_height()//2))
         self.graph.blit(minx_label, (70 - minx_label.get_width()//2, self.height-(60+((self.height-70)/2))- minx_label.get_height()//2))
         self.graph.blit(maxx_label, (self.width-30 - maxx_label.get_width()//2, self.height-(60+((self.height-70)/2))-maxx_label.get_height()//2))
